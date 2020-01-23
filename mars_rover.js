a34d2260d61705dc7efa41d5dee8461a15b410e6
Felipe Massao <massao.felipe@gmail.com>
2020-01-23 18:09:32 -0300

    moveForward function implemented

--- mars_rover.js
+++ mars_rover.js
@@ -2,11 +2,18 @@
 // ======================
 
 const DIRECTIONS = ["N", "E", "S", "W", "N"];
+const MOVEMENT = {
+    N : {x: 0, y:-1},
+    E : {x: 1, y: 0},
+    S : {x: 0, y: 1},
+    W : {x: -1, y: 0}
+  };
 
-let rover = {
-    direction : "N"
+  let rover = {
+    direction : "N",
+    x : 0,
+    y : 0
 };
-
 
 // ======================
 function turnLeft(rover){
@@ -22,6 +29,9 @@ function turnLeft(rover){
   }
   
   function moveForward(rover){
+    let movement = MOVEMENT[rover.direction];
+    rover.x += movement.x;
+    rover.y += movement.y;
     console.log("moveForward was called")
   }
   
