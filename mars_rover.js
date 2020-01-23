74e9b7d0d66d4b3ba51a554698375eadb684c389
Felipe Massao <massao.felipe@gmail.com>
2020-01-23 17:35:59 -0300

    turnLeft and turnRight functions implemented

--- mars_rover.js
+++ mars_rover.js
@@ -1,15 +1,23 @@
 // Rover Object Goes Here
 // ======================
 
+const DIRECTIONS = ["N", "E", "S", "W", "N"];
 
+let rover = {
+    direction = "N"
+};
 
 
 // ======================
 function turnLeft(rover){
+    let indexRoverDirection = DIRECTIONS.lastIndexOf(rover.direction);
+    rover.direction = DIRECTIONS[indexRoverDirection - 1];
     console.log("turnLeft was called!");
   }
   
   function turnRight(rover){
+    let indexRoverDirection = DIRECTIONS.indexOf(rover.direction);
+    rover.direction = DIRECTIONS[indexRoverDirection + 1];
     console.log("turnRight was called!");
   }
   
