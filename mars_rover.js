6c44695ef63f65c8f81ab8bbec2927dbef7b538a
Felipe Massao <massao.felipe@gmail.com>
2020-01-23 16:49:54 -0300

    Initial commit for mars rover

---
+++ mars_rover.js
@@ -0,0 +1,20 @@
+// Rover Object Goes Here
+// ======================
+
+
+
+
+// ======================
+function turnLeft(rover){
+    console.log("turnLeft was called!");
+  }
+  
+  function turnRight(rover){
+    console.log("turnRight was called!");
+  }
+  
+  function moveForward(rover){
+    console.log("moveForward was called")
+  }
+  
+  
